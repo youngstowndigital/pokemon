9fbc0e3e722028bfa19916ac5c23049fbb3451f7
Mike Wittenauer <mikewittenauer@me.com>
2021-10-10 16:48:43 -0400

    Install react bootstrap

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
 
 const PokemonCard = ({ pokemon: { name } }) => {
     return (
-        <div>
-            <h1>{ name }</h1>
-        </div>
+        <Card className="m-3">
+            <Card.Body>
+                <Card.Title>{ name }</Card.Title>
+            </Card.Body>
+        </Card>
     )
 };
 
