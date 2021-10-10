8f2d56ca7730284ca8e6b584bc7b97f1a1189257
Mike Wittenauer <mikewittenauer@me.com>
2021-10-10 00:24:21 -0400

    Get react to work with babel/webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,20 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
     },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            }
+        ],
+    },
     devServer: {
         static: {
             directory: path.join(__dirname, 'public'),
